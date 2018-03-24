b567eeee894df258db1016b76775e7da49f083e0
khecaqs <khecaqs@gmail.com>
2018-03-24 23:20:32 +0800

    1st master

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:login_app/auth.dart';
+import 'package:login_app/routes.dart';
+
+void main() => runApp(new LoginApp());
+
+class LoginApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'My Login App',
+      theme: new ThemeData(
+        primarySwatch: Colors.red
+      ),
+      routes: routes,
+    );
+  }
+}
---
+++ lib/screens/login/login_screen_presenter.dart
@@ -0,0 +1,19 @@
+import 'package:login_app/data/rest_ds.dart';
+import 'package:login_app/models/user.dart';
+
+abstract class LoginScreenContract {
+  void onLogginSuccess(User user);
+  void onLogginError(String errorTxt);
+}
+
+class LoginScreenPresenter {
+  LoginScreenContract _view;
+  RestDataSource api = new RestDataSource();
+
+  LoginScreenPresenter(this._view);
+  doLogin(String username, String password) {
+    api.login(username, password).then((User user){
+      _view.onLogginSuccess(user);
+    }).catchError((Exception error)=> _view.onLogginError(error.toString()));
+  }
+}
